632a1a86e65f0b786050a955688793f97274b6c0
Heezyyy <85496398+Heezyyy@users.noreply.github.com>
2024-08-20 17:11:01 -0300

    Add files via upload

---
+++ OPERATOR.js
@@ -0,0 +1,64 @@
+//declarando as váriaveis
+var num01 = 200
+var num02 = 150
+var num03 = 100
+
+//soma
+console.log("Todas somas entre\n " + num01 + ", " + num02 + " e " + num03);
+
+console.log(); //console.log() vazio para quebra de linha.
+                                                     //lógica
+console.log("A soma de todos números é: " + (num01 + num02 + num03));
+console.log("A soma dos dois primeiros números é: " + (num01 + num02));             
+console.log("A soma do primeiro e último número é: " + (num01 + num03));
+console.log("A soma dos dois últimos números é: " + (num02 + num03));
+
+//subtração
+console.log("-----------------------------");
+
+console.log("Todas subtrações entre\n " + num01 + ", " + num02 + " e " + num03);
+
+console.log();
+                                                         //lógica
+console.log("A subtração de todos números é: " + (num01 - num02 - num03));
+console.log("A subtração dos dois primeiros números é: " + (num01 - num02));
+console.log("A subtração do primeiro e último número é: " + (num01 - num03));
+console.log("A subtração dos dois últimos números é: " + (num02 - num03));
+
+//multiplicação
+console.log("-----------------------------");
+
+console.log("Todas multiplicações entre\n " + num01 + ", " + num02 + " e " + num03);
+
+console.log();
+                                                              //lógica
+console.log("A multiplicação de todos números é: " + (num01 * num02 * num03));
+console.log("A multiplicação dos dois primeiros números é: " + (num01 * num02));
+console.log("A multiplicação do primeiro e último número é: " + (num01 * num03));
+console.log("A multiplicação dos dois últimos números é: " + (num02 * num03));
+
+//divisão
+console.log("-----------------------------");
+
+console.log("Todas divisões entre\n " + num01 + ", " + num02 + " e " + num03);
+
+console.log();
+                                                         //lógica
+console.log("A divisão de todos números é: " + (num01 / num02 / num03));
+console.log("A divisão dos dois primeiros números é: " + (num01 / num02));
+console.log("A divisão do primeiro e último número é: " + (num01 / num03));
+console.log("A divisão dos dois últimos números: " + (num02 / num03));
+
+//exponenciação
+/* console.log("-----------------------------");
+
+console.log("Todas exponenciações entre\n " + num01 + ", " + num02 + " e " + num03);
+
+console.log();
+                                                              //lógica
+console.log("A exponenciação de todos números é: " + (num01 ** num02 ** num03));
+console.log("A exponenciação dos dois primeiros números: " + (num01 ** num02));
+console.log("A exponenciação do primeiro e último número é: " + (num01 ** num03));
+console.log("A exponenciação dos dois últimos números: " + (num02 ** num03));
+
+console.log("-----------------------------"); */
